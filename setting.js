84397175e7699cb70ade3d356726373f05ceb4bf
sameerastixc <sameerastixc@gmail.com>
2026-02-08 13:45:45 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𓆩 𝐒𝐀𝐌𝐄𝐄𝐑-𝐌𝐃 𓆪',
+  author: '‎',
+  botName: "𓆩 𝐒𝐀𝐌𝐄𝐄𝐑-𝐌𝐃 𓆪",
+  botOwner: 'SAMEER', // Your name
+  ownerNumber: '923261136315', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/sameerastixc/SAMEER-MD",
+};
+
+module.exports = settings;
